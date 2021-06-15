94a4ee8bc9975eb4eb7b23d09ccf8f9bf5907867
Matt Autrey <mautrey17@gmail.com>
2021-06-15 10:25:38 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import useState from 'react';
+
+function App() {
+
+  const [count, setCount] = React.useState(0);
+
+
+  return (
+    <div className="App">
+      <h1>test</h1>
+      <p>
+        {count}
+      </p>
+      <button onClick={() => setCount(count + 1)}>Stop the count</button>
+    </div>
+  );
+}
+
+export default App;
